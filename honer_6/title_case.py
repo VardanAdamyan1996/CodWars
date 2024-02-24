cdd1ea639eb79fadd9c705ed918b0c3cf33ffef4
Vardan Adamyan <vardan47@gmail.com>
2024-02-24 23:57:51 +0400

    add new line

--- honer_6/title_case.py
+++ honer_6/title_case.py
@@ -3,8 +3,10 @@ def title_case(titel: str, minor_words: str = '') -> str:
     minor_words = minor_words.split()
     return ' '.join([word if word in minor_words else word.capitalize() for word in titel])
 
+
 minor_words = 'the of an'
 string = 'a chash of kings'
 # print(string.split())
 result = title_case(titel=string, minor_words=minor_words)
 print(result)
+x = 12 * 3
